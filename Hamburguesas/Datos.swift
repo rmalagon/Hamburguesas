ef3ff5032597ea430d702275e157cdfdfd58441d
Ricardo Adolfo Malagón Ganado <Ricardo@macbook-pro-de-ricardo.local>
2016-02-29 21:21:35 +0100

    Initial Commit

---
+++ Hamburguesas/Datos.swift
@@ -0,0 +1,9 @@
+//
+//  Datos.swift
+//  Hamburguesas
+//
+//  Created by Ricardo Adolfo Malagón Ganado on 29/2/16.
+//  Copyright © 2016 Ricardo Adolfo Malagón Ganado. All rights reserved.
+//
+
+import Foundation
